833f04ef38e2faa586c15df750d85edfd2869689
Miguel Lopez <lopezpedres@gmail.com>
2021-04-05 13:40:04 -0700

    1ER Commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+#create the app and app's name
+app = Flask(__name__)
+
+#if dev, it's development mode. Which means that we are going to store ur data in postgres
+ENV = 'dev'
+if ENV=='dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123Aapostgres@localhost/lexus'
+
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+#We dont want to keep track of this if we dont need it, requires extra memory
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+
+#creates our db, which works with models
+db = SQLAlchemy(app)
+
+class feedBack(db.Model):
+    __tablename__ = 'Feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique = True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self,customer,dealer,rating,comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
+
+
+#the "/" means that is ony for the home page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+"""le damos la ruta a la que queremos llegar, dentro de index.html hay un tag que se llama
+form, dentro hay una accion y un metodo los cuales vamos a usar """
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method =='POST':
+        customer = request.form["customer"]
+        dealer = request.form["dealer"]
+        rating = request.form["rating"]
+        comments = request.form["comments"]
+        #print(customer,dealer, rating, comments)
+        
+        if customer == '' or dealer == '':
+            return render_template('index.html', message= 'Please enter required fields')
+        if db.session.query(feedBack).filter(feedBack.customer==customer).count()==0:
+            data=feedBack(customer,dealer,rating,comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+        else: 
+             return render_template('index.html', message= 'You have al ready submitted feedback')
+#Esto es para que el programa unicamente corra cuando llamemos a programa.       
+if __name__=='__main__':
+    app.debug = True
+    app.run()
